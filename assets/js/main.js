408d2b0437077e0e2b68f7880da1e13629db91cb
pro16 <matteo.porrani@gmail.com>
2022-01-20 16:49:34 +0100

    base 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,62 @@
 
-const continents = ['Europe', 'Afrique', 'Asie', 'Amériques', 'Océanie'];
-let currContinent;
+const continentsList = ['Europe', 'Afrique', 'Asie', 'Amériques', 'Océanie'];
+const continentsListEng = ['Europe', 'Africa', 'Asia', 'Americas', 'Oceania'];
+
+let currContinent ='Europe';
 const heroTitle = document.querySelector('.hero__title h2');
+const countriesUl = document.querySelector('.countries__list');
 
-document.hero__menu.continent.addEventListener('change', (e) => {
+// gestionnaire d'événement <select>
+document.hero__menu.continent.addEventListener('change', () => {
 
-  currContinent = continents[parseInt(document.hero__menu.continent.value)];
+  currContinent = continentsList[parseInt(document.hero__menu.continent.value)];
+  currContinentEng = continentsListEng[parseInt(document.hero__menu.continent.value)];
+
+  refreshContinent(currContinentEng);
   heroTitle.textContent = currContinent;
-
 });
+
+
+
+function refreshContinent(continentName) {
+
+  fetch('https://restcountries.com/v3.1/all')
+  .then(res => res.json())
+  .then(data => {
+    // console.log(data);
+
+    const filteredData = data.filter(country => {
+      if (country.region === continentName) {
+        return country;
+      }
+    })
+    .sort((a, b) => {
+      return (a.translations.fra.common > b.translations.fra.common) ? 1 : -1;
+    });
+
+    console.log(filteredData);
+
+    if (filteredData) {
+      displayCountries(filteredData)
+    }
+
+  });
+}
+
+
+
+function displayCountries(countryList) {
+
+  let countriesLis = [];
+
+  countryList.forEach(country => {
+    const countryLi = `<li class="country">${country.translations.fra.common}</li>`;
+    countriesLis.push(countryLi);
+  });
+
+  countriesUl.innerHTML = countriesLis.join('');
+}
+
+// Initialise la page avec currContinent ='Europe'
+refreshContinent(currContinent);
+

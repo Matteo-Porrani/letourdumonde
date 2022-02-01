088d24787ac3b6f399f175fe4f9ae02e4378ff7c
pro16 <matteo.porrani@gmail.com>
2022-02-01 23:05:24 +0100

    structure complétée et palette de test

---
+++ assets/js/page2toggler.js
@@ -0,0 +1,23 @@
+
+// const page2CountryList = document.querySelector('.page2__country__list');
+// déjà déclaré...
+
+const page2toggler = document.querySelector('.toggler--page2');
+
+page2toggler.addEventListener('click', (e) => {
+  
+  console.log(e.target.classList);
+
+  if (e.target.dataset.state === 'closed') {
+    e.target.dataset.state = 'open';
+    page2CountryList.classList.remove('country__list--hidden');
+  } else if (e.target.dataset.state === 'open') {
+    e.target.dataset.state = 'closed';
+    page2CountryList.classList.add('country__list--hidden');
+  }
+
+
+});
+
+
+
